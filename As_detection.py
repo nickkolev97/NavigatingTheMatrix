fixed the normalisation of the As detection for the defect crops

As_detection.py
@@ -768,11 +768,21 @@ class Detector(object):
         '''
         recentre mean to 0. Used for the window classifiers
         '''
+        if len(array.shape)==3:
+            mean_f = torch.mean(array[0,:,:])
+            mean_e = torch.mean(array[1,:,:])
+            std_f = torch.std(array[0,:,:])
+            std_e = torch.std(array[1,:,:])
+            array[0,:,:] = (array[0,:,:] - mean_f)/std_f
+            array[1,:,:] = (array[1,:,:] - mean_e)/std_e
         if len(array.shape)==4:
             mean_f = torch.mean(array[0,0,:,:] )
             mean_e = torch.mean(array[0,1,:,:] )
-            array[0,0,:,:] = (array[0,0,:,:] - mean_f)#/torch.std(array[0,0,:,:])
-            array[0,1,:,:] = (array[0,1,:,:] - mean_e)#/torch.std(array[0,1,:,:])
+            std_f = torch.std(array[0,0,:,:] )
+            std_e = torch.std(array[0,1,:,:] )
+            array[0,0,:,:] = (array[0,0,:,:] - mean_f)/std_f
+            array[0,1,:,:] = (array[0,1,:,:] - mean_e)/std_e
+        
         return array
 
     def norm2(self, images):
@@ -1026,44 +1036,47 @@ class Detector(object):
             
         coord =  coord_f.copy() - self.crop_size
 
-       
-        #distances = np.sqrt(np.sum((coord-self.crop_size-DVcoords.T)**2, axis=1))
-        #if (distances>min_dist).all():
-        # if you want to not include the ones that are too close to DVs then uncomment the above lines 
-        # and the last three in this method
-
-
         window = np.transpose(window, (0,3,1,2))
         self.windows.append(window)
         window = self.norm1_(torch.tensor(window).float())
-        #print(window.shape)
-        #print('np mean 0:', np.mean(window[0,:,:,0]))
-        #print('np mean 1:', np.mean(window[0,:,:,1]))
-        # normalise
-        #window = torch.tensor(window).float()
-       # print('torch mean 0:', torch.mean(window[0,:,:,0]))
-       # print('torch mean 1:', torch.mean(window[0,:,:,1]))
         
-        #plt.imshow(window[0,0,:,:], cmap='afmhot')
-        #plt.show()
+        '''
+        # plot the full scan with a square around the crop that was taken
+        mask = np.zeros(scan_filled.shape)
+        x = coord[1] #- self.crop_size
+        y = coord[0] #- self.crop_size
+        mask[y-7:y+7,x+7:x+9] = 0.5
+        mask[y-7:y+7,x-9:x-7] = 0.5
+        mask[y+7:y+9,x-7:x+7] = 0.5
+        mask[y-9:y-7,x-7:x+7] = 0.5
+        fig, ax = plt.subplots(1,4,figsize=(10,10))
+        ax[0].imshow(scan_filled+mask, cmap='afmhot')
+        ax[1].imshow(scan_empty+mask, cmap='afmhot')
+        ax[2].imshow(window[0,0,:,:], cmap='afmhot')
+        ax[3].imshow(window[0,1,:,:], cmap='afmhot')
+        plt.show()
+        '''
+
         if si_scan.As:
-            # for ensemble model
             torch.manual_seed(0)
             y = self.model_As(window)
         elif not si_scan.As:     
             torch.manual_seed(0)
             y = self.model_DB(window)
+#        print(y)
         prediction = torch.argmax(y)+1
+ #       print(prediction)
         if prediction == 1:
             si_scan.feature_coords['oneDB'].append(coord-self.crop_size)
         elif prediction == 2:
             si_scan.feature_coords['twoDB'].append(coord-self.crop_size)
         elif prediction == 3:
             si_scan.feature_coords['anomalies'].append(coord-self.crop_size)
-        #elif prediction==4 it's lattice (i.e UNet probably made wrong prediction)
         elif prediction == 5:
             si_scan.feature_coords['As'].append(coord-self.crop_size)      
+        
 
+        
         #else:
         #    si_scan.feature_coords['closeToDV'].append(coord-self.crop_size)
         #    prediction = 8 
@@ -2748,34 +2761,71 @@ class scan_stitcher(object):
 
 
 if __name__ == "__main__":
-    cwd = Path.cwd() # current working directory
-    path = cwd / 'examples' / '20181123-122007_STM_AtomManipulation-Earls Court-Si(100)-H term--26_1.Z_mtrx'
-    # make an STM object from NavigatingTheMatrix file
-    scan_dict = {'file': str(path), 
-                'standard_pix_ratio': 1024/200
-                }
-    scan = nvm.STM(scan_dict)
-    # clean up the scans
-    scan.clean_up(scan.trace_down, 'trace down', plane_level=True)
-    scan.clean_up(scan.retrace_down, 'retrace down', plane_level=True)
-    # correct hysteresis
-    scan.trace_down_proc, scan.retrace_down_proc, corrected, k = scan.correct_hysteresis(scan.trace_down_proc, scan.retrace_down_proc, 'trace down')
-    # make a Si_scan object just for trace down
-    trace_down = Si_Scan(scan, 'trace down', As=True)
-    # make detector object to find and label defects
-    detector = Detector()
-    # run prediction
-    trace_down.one_hot_segmented = detector.predict(trace_down)
-    # turn output into rgb image
-    trace_down.rgb = detector.turn_rgb(trace_down.one_hot_segmented)
-    ic(trace_down.feature_coords['As'])
-   # np.save('ec_features.npy', detector.windows)
-    # find distances between features
-   # trace_down.feature_dists()
-    # filter for distances and certain feature types
-    #trace_down.find_pairs('As', 'As', max_dist = 50, min_dist =0,angle=[(70,120)], display_image=True)
-    #trace_down.find_pairs('As', 'As', max_dist = 50, min_dist =0,angle=[(350,20)], display_image=True)
-    #trace_down.find_pairs('As', 'oneDB', max_dist = 15, min_dist = 10, angle=[(0,90)], display_image=True)
-    # show final segmentation
-    plt.imshow(trace_down.rgb)
-    plt.show()
+    img = 1
+    
+    if img ==1:
+        cwd = Path.cwd() # current working directory
+        path = cwd / 'examples' / '20181123-122007_STM_AtomManipulation-Earls Court-Si(100)-H term--26_1.Z_mtrx'
+        # make an STM object from NavigatingTheMatrix file
+        scan_dict = {'file': str(path), 
+                    'standard_pix_ratio': 1024/200
+                    }
+        scan = nvm.STM(scan_dict)
+        # clean up the scans
+        scan.clean_up(scan.trace_down, 'trace down', plane_level=True)
+        scan.clean_up(scan.retrace_down, 'retrace down', plane_level=True)
+        # correct hysteresis
+        scan.trace_down_proc, scan.retrace_down_proc, corrected, k = scan.correct_hysteresis(scan.trace_down_proc, scan.retrace_down_proc, 'trace down')
+        # make a Si_scan object just for trace down
+        trace_down = Si_Scan(scan, 'trace down', As=True)
+        # make detector object to find and label defects
+        detector = Detector()
+        # run prediction
+        trace_down.one_hot_segmented = detector.predict(trace_down)
+        # turn output into rgb image
+        trace_down.rgb = detector.turn_rgb(trace_down.one_hot_segmented)
+        ic(trace_down.feature_coords['As'])
+    # np.save('ec_features.npy', detector.windows)
+        # find distances between features
+    # trace_down.feature_dists()
+        # filter for distances and certain feature types
+        #trace_down.find_pairs('As', 'As', max_dist = 50, min_dist =0,angle=[(70,120)], display_image=True)
+        #trace_down.find_pairs('As', 'As', max_dist = 50, min_dist =0,angle=[(350,20)], display_image=True)
+        #trace_down.find_pairs('As', 'oneDB', max_dist = 15, min_dist = 10, angle=[(0,90)], display_image=True)
+        # show final segmentation
+        plt.imshow(trace_down.rgb)
+        plt.show()
+     
+    
+    elif img ==2:
+        cwd = Path.cwd() # current working directory
+        path = cwd / 'examples' / '20200212-183707_Hainault-Si(001)-H--54_1.Z_mtrx'
+        # make an STM object from NavigatingTheMatrix file
+        scan_dict = {'file': str(path), 
+                    'standard_pix_ratio': 1024/200
+                    }
+        scan = nvm.STM(scan_dict)
+        # clean up the scans
+        scan.clean_up(scan.trace_up, 'trace up', plane_level=True)
+        scan.clean_up(scan.retrace_up, 'retrace up', plane_level=True)
+        # correct hysteresis
+        scan.trace_up_proc, scan.retrace_up_proc, corrected, k = scan.correct_hysteresis(scan.trace_up_proc, scan.retrace_up_proc, 'trace up')
+        # make a Si_scan object just for trace down
+        trace_up = Si_Scan(scan, 'trace up', As=True)
+        # make detector object to find and label defects
+        detector = Detector()
+        # run prediction
+        trace_up.one_hot_segmented = detector.predict(trace_up)
+        # turn output into rgb image
+        trace_up.rgb = detector.turn_rgb(trace_up.one_hot_segmented)
+        ic(trace_up.feature_coords['As'])
+    # np.save('ec_features.npy', detector.windows)
+        # find distances between features
+    # trace_up.feature_dists()
+        # filter for distances and certain feature types
+        #trace_up.find_pairs('As', 'As', max_dist = 50, min_dist =0,angle=[(70,120)], display_image=True)
+        #trace_up.find_pairs('As', 'As', max_dist = 50, min_dist =0,angle=[(350,20)], display_image=True)
+        #trace_up.find_pairs('As', 'oneDB', max_dist = 15, min_dist = 10, angle=[(0,90)], display_image=True)
+        # show final segmentation
+        plt.imshow(trace_up.rgb)
+        plt.show()